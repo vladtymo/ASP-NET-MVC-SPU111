2132c1b69adfb9da18c6a83804a74c1445aa806f
vladtymo <tymo.vlad@gmail.com>
2023-09-09 10:32:00 +0300

    Add project files.

---
+++ AspNet_MVC_SPU111/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using AspNet_MVC_SPU111.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AspNet_MVC_SPU111.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View(); // ~/Views/Home/Index.cshtml
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ AspNet_MVC_SPU111/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspNet_MVC_SPU111.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
