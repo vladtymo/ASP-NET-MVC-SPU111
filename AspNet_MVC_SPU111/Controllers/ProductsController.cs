7279f384388617b05c170b397187467f18026d8c
vladtymo <tymo.vlad@gmail.com>
2023-09-16 11:13:09 +0300

    Implement delete button.

--- AspNet_MVC_SPU111/Controllers/ProductsController.cs
+++ AspNet_MVC_SPU111/Controllers/ProductsController.cs
@@ -8,11 +8,25 @@ namespace AspNet_MVC_SPU111.Controllers
     {
         ShopSPUDbContext ctx = new ShopSPUDbContext();
 
+        // Get all products
         public IActionResult Index()
         {
             var products = ctx.Products.ToList();
 
             return View(products);
+        }
+
+        // Delete product by ID
+        public IActionResult Delete(int id)
+        {
+            var item = ctx.Products.Find(id);
+
+            if (item == null) return NotFound();
+
+            ctx.Products.Remove(item);
+            ctx.SaveChanges(); // delete from db
+
+            return RedirectToAction("Index");
         }
     }
 }
